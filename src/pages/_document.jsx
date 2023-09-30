0e3fca492f02c5d02c12d7c993dcc7ce79905a69
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-09-30 21:47:49 +0900

    ✨ #2 PWAを実装

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="application-name" content="HeyaGram" />
+        <meta name="theme-color" content="#fff" />
+        <link rel="icon" sizes="128x128" href="/icon_128x128.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body>
         <Main />
         <NextScript />
