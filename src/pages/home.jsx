7ec48de8a6cb5194c33257709ce5d57f4d0601f6
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-10-02 20:25:27 +0900

    ✅ #7 テストの削除

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,27 +1,10 @@
-import React, { useEffect } from 'react'
-import { collection, getDocs, onSnapshot } from 'firebase/firestore';
-import { db } from '@/lib/firebase';
+import React from 'react'
 import MainContainer from '@/components/mainContainer';
 
 function home() {
-    const [ posts, setPosts ] = React.useState([]);
-    useEffect(() => {
-        const postData = collection(db, 'posts');
-        getDocs(postData).then((snapshot) => {
-            setPosts(snapshot.docs.map((doc) => doc.data()));
-            onSnapshot(postData, (post) => {
-                setPosts(post.docs.map((doc) => doc.data()));
-            });
-        });
-    }, []);
     return (
         <MainContainer>
             {/* コンテンツ */}
-            { posts.map((post) => (
-                <div key={post.username}>
-                    <p>{post.body}</p>
-                </div>
-            ))}
         </MainContainer>
     )
 }
