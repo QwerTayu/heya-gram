33cacf87bff35d07b8ab1200e63022c4ee9f44c2
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-10-02 19:16:16 +0900

    ✅ #7 firebaseのテスト

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,28 @@
 import NavBar from '@/pages/components/navbar'
-import React from 'react'
+import React, { useEffect } from 'react'
 import MainContainer from '@/pages/components/mainContainer'
+import { collection, getDocs, onSnapshot } from 'firebase/firestore';
+import { db } from '@/pages/lib/firebase';
 
 function home() {
+    const [ posts, setPosts ] = React.useState([]);
+    useEffect(() => {
+        const postData = collection(db, 'posts');
+        getDocs(postData).then((snapshot) => {
+            setPosts(snapshot.docs.map((doc) => doc.data()));
+            onSnapshot(postData, (post) => {
+                setPosts(post.docs.map((doc) => doc.data()));
+            });
+        });
+    }, []);
     return (
         <MainContainer>
             {/* コンテンツ */}
+            { posts.map((post) => (
+                <div key={post.username}>
+                    <p>{post.body}</p>
+                </div>
+            ))}
         </MainContainer>
     )
 }
