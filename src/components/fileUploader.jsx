29ccaffc01c99715af1cdf5b490c07c745da5de6
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-10-04 00:15:09 +0900

    💄 #8 テキストエリアのスタイリングを行いました

--- src/components/fileUploader.jsx
+++ src/components/fileUploader.jsx
@@ -22,17 +22,13 @@ function FileUploader({ postId, setIsFileUploaded }) {
             fetchImageURL();
         }
     }, [isUploaded]);
-
-    useEffect(() => {
-        // imageURL が更新されたら画像を表示する
-    }, []);
     
     return (
         <>
             <>
             {isUploading ? (
                 <> {/* アップロード中 */}
-                    <div className="flex items-center justify-center w-full">
+                    <div className="flex flex-none items-center justify-center w-full">
                         <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-70 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                             <div className="flex flex-col items-center justify-center pt-5 pb-6">
                                 <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
@@ -47,7 +43,7 @@ function FileUploader({ postId, setIsFileUploaded }) {
                 <>
                     {isUploaded ? (
                         <> {/* アップロード完了 */}
-                            <div className="flex items-center justify-center w-full">
+                            <div className="flex flex-none items-center justify-center w-full">
                                 <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-70 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                                     <img src={imageURL} alt="hello"></img>
                                 </label>
@@ -55,7 +51,7 @@ function FileUploader({ postId, setIsFileUploaded }) {
                         </>
                     ) : (
                         <> {/* アップロードしてない */}
-                            <div className="flex items-center justify-center w-full">
+                            <div className="flex flex-none items-center justify-center w-full">
                                 <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                                     <div className="flex flex-col items-center justify-center pt-5 pb-6">
                                         <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
--- src/pages/create.jsx
+++ src/pages/create.jsx
@@ -37,7 +37,7 @@ function create() {
                 <button type='button' onClick={() => router.back()} className='p-2'>
                     <PiArrowLeftBold size={24} />
                 </button>
-                {isFileUploaded ? (
+                {isFileUploaded && body ? (
                     <button type='button' onClick={() => sendPost()} className='p-2'>
                         <PiPaperPlaneTiltBold size={24} />
                     </button>
@@ -47,8 +47,10 @@ function create() {
                     </button>
                 )}
             </div>
-            <FileUploader postId={postId} setIsFileUploaded={setIsFileUploaded}/>
-            <input type="text" placeholder="掃除をしよう！" value={body} onChange={(e) => handleInputChange(e.target.value)} />
+            <div className='flex flex-col justify-between h-full'>  
+                <FileUploader postId={postId} setIsFileUploaded={setIsFileUploaded}/>
+                <textarea id="message" placeholder="掃除をしよう！" value={body} onChange={(e) => handleInputChange(e.target.value)} rows="4" className="grow block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 resize-none overflow-auto"></textarea>
+            </div>
         </MainContainer>
     )
 }
