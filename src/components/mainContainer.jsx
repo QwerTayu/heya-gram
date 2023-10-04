03430a757d896cdf908120c2355960aa7532d3e1
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-10-04 22:40:00 +0900

    ✨ #5 スクロールできるようにしました

--- src/components/mainContainer.jsx
+++ src/components/mainContainer.jsx
@@ -5,7 +5,7 @@ function MainContainer({ children }) {
   return (
     <div className='max-w-[600px] w-[100%] mx-auto bg-gray-50 h-screen flex flex-col justify-between'>
       <div className='bg-gray-200 py-4 flex justify-center h-[56px]'>部屋ぐらむ</div>
-      <div className='h-full justify-start flex flex-col'>
+      <div className='overflow-y-auto h-full justify-start flex flex-col'>
         { children }
       </div>
       <NavBar />
