ef6370335f575066077932c508900dfa3446a480
hengin-eer <hirokix0608@gmail.com>
2023-10-04 20:52:46 +0900

    ✨ feat: #19 グローバルステートを管理できるように

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,15 +1,18 @@
 import { GetCurrentUser } from '@/components/getCurrentUser'
 import '@/styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
+import { RecoilRoot } from 'recoil'
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps }
 }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-      <GetCurrentUser />
-    </SessionProvider>
+    <RecoilRoot>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+        <GetCurrentUser />
+      </SessionProvider>
+    </RecoilRoot>
   )
 }
