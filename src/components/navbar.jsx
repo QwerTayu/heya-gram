4d916cd2831bc641385c3ea4aaf6925420ec9392
hengin-eer <hirokix0608@gmail.com>
2023-10-04 20:45:22 +0900

    🐛 fix: ページ遷移をaタグからNextLinkに変更

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,26 +1,27 @@
+import Link from 'next/link';
 import React from 'react'
 import { PiBellBold, PiCameraPlusBold, PiHouseBold, PiMagnifyingGlassBold, PiUserBold } from "react-icons/pi";
 
 function NavBar() {
     return (
         <nav className="bg-gray-200 py-4 w-full h-[56px]">
-                <div className="container mx-auto flex items-center justify-around">
-                <a href="/home" className="text-black">
+            <div className="container mx-auto flex items-center justify-around">
+                <Link href="/home" className="text-black">
                     <PiHouseBold size={24} />
-                </a>
-                <a href="#" className="text-black">
+                </Link>
+                <Link href="#" className="text-black">
                     <PiMagnifyingGlassBold size={24} />
-                </a>
-                <a href="/create" className="text-black">
+                </Link>
+                <Link href="/create" className="text-black">
                     <PiCameraPlusBold size={24} />
-                </a>
-                <a href="#" className="text-black">
+                </Link>
+                <Link href="#" className="text-black">
                     <PiBellBold size={24} />
-                </a>
-                <a href="/username" className="text-black">
+                </Link>
+                <Link href="/username" className="text-black">
                     <PiUserBold size={24} />
-                </a>
-                </div>
+                </Link>
+            </div>
         </nav>
     )
 }
