d8d43be765ca44333ed8c45d17baa1484e41735f
hengin-eer <hirokix0608@gmail.com>
2023-10-06 12:01:37 +0900

    ✨ feat: #プロフィールページからユーザーページに変更

--- src/pages/users/[username].jsx
+++ src/pages/users/[username].jsx
@@ -1,20 +1,37 @@
 import MainContainer from '@/components/mainContainer'
+import { useFirestore } from '@/hooks/useFirestore'
 import { currentUserState } from '@/states/currentUserState'
 import { signOut } from 'next-auth/react'
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect, useMemo, useState } from 'react'
 import { useRecoilValue } from 'recoil'
 
 function username() {
     const currentUser = useRecoilValue(currentUserState)
+    const [profileUser, setProfileUser] = useState(null);
+    const { getProfileUserById } = useFirestore();
+
+    const router = useRouter();
+    const routerUserId = router.query.username;
+
+    useMemo(async () => {
+        if (router.isReady && currentUser) {
+            if (routerUserId === currentUser.uid) setProfileUser(currentUser);
+            else {
+                const user = await getProfileUserById(routerUserId);
+                setProfileUser(user)
+            }
+        }
+    }, [router, currentUser])
 
     return (
         <MainContainer>
-            {currentUser && (
+            {profileUser && (
                 <div className='w-full px-5 py-2'>
                     <div className='flex gap-5 items-center'>
-                        <img className='w-[60px] h-[60px] rounded-full' src={currentUser.image} alt={currentUser.name} />
+                        <img className='w-[60px] h-[60px] rounded-full' src={profileUser.image} alt={profileUser.name} />
                         <div className='w-full'>
-                            <h1 className='mb-3 text-xl font-bold'>{currentUser.name}</h1>
+                            <h1 className='mb-3 text-xl font-bold'>{profileUser.name}</h1>
                             <button className='w-full px-5 py-1 text-white bg-cyan-400 rounded-full
                                 hover:bg-cyan-500 transition duration-300 ease-in-out'
                             >
@@ -42,12 +59,14 @@ function username() {
                         </div>
                     </div>
 
-                    <button className='block px-5 py-2 mx-auto mt-5 rounded-3xl text-base text-white bg-red-500
+                    {profileUser.uid === currentUser.uid && (
+                        <button className='block px-5 py-2 mx-auto mt-5 rounded-3xl text-base text-white bg-red-500
                         hover:bg-red-600 transition duration-300 ease-in-out'
-                        onClick={() => signOut({ redirect: true, callbackUrl: '/' })}
-                    >
-                        ログアウトする
-                    </button>
+                            onClick={() => signOut({ redirect: true, callbackUrl: '/' })}
+                        >
+                            ログアウトする
+                        </button>
+                    )}
                 </div>
             )}
         </MainContainer>
