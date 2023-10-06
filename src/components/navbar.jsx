6e4dc1da5cf6ac03e7b85483ec9ef0115a55dfdd
hengin-eer <hirokix0608@gmail.com>
2023-10-06 12:30:42 +0900

    🐛 fix: #27 ナビゲーションのリンク先の変更

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,19 @@
+import { currentUserState } from '@/states/currentUserState';
 import Link from 'next/link';
 import React from 'react'
 import { PiBellBold, PiCameraPlusBold, PiHouseBold, PiMagnifyingGlassBold, PiUserBold } from "react-icons/pi";
+import { useRecoilValue } from 'recoil';
 
 function NavBar() {
+    const currentUser = useRecoilValue(currentUserState);
+
     return (
         <nav className="bg-gray-200 py-4 w-full h-[56px]">
             <div className="container mx-auto flex items-center justify-around">
                 <Link href="/home" className="text-black">
                     <PiHouseBold size={24} />
                 </Link>
-                <Link href="#" className="text-black">
+                <Link href="/search" className="text-black">
                     <PiMagnifyingGlassBold size={24} />
                 </Link>
                 <Link href="/create" className="text-black">
@@ -18,7 +22,7 @@ function NavBar() {
                 <Link href="#" className="text-black">
                     <PiBellBold size={24} />
                 </Link>
-                <Link href="/username" className="text-black">
+                <Link href={currentUser ? `/users/${currentUser.uid}`: '/users'} className="text-black">
                     <PiUserBold size={24} />
                 </Link>
             </div>
